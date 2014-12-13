e796f06760684809d6fc9418179d41e208b62492
Bryce Matsuda <iexist4jcore@gmail.com>
2014-12-12 21:51:06 -1000

    Begin basic m3u validation.

--- playlistShuffler.rb
+++ playlistShuffler.rb
@@ -8,8 +8,8 @@ The equivalent to shuffling any playlist/library in iTunes or Banshee.
 Except you are given the entire list, not just the next 10/20/30 songs in the list.
 Because some people might want to know what the entire play order is.
 
-Only .m3u files are accepted right now. For now, the program assumes the m3u is already valid. 
-Actual .m3u validation may be implemented at a later date.
+Only extended m3u files are accepted right now. For now, the program assumes the m3u is already valid. 
+Actual extended m3u validation may be implemented at a later date.
 
 Command line format: ruby playlistShuffler.rb [input.m3u] [optionalOutputName.m3u]
 
@@ -50,13 +50,21 @@ songPile = Array.new
 # Counter
 x = 0
 
+# Check for invalid extended m3u files
+
+# Valid extended m3u's must start with #EXTM3U
+if file.eof? or !file.readline.eql? "#EXTM3U\n"
+  puts("Error: #{fileInput} must start with #EXTM3U to be a valid .m3u file")
+  exit
+end
+
 # Save all lines except for the first one
 # (which we'll add later to the new .m3u file)
 while !file.eof?
  line = file.readline
  if !line.eql? "#EXTM3U\n"
-    songs.push(line)
-  end
+  songs.push(line)
+end
 end
 
 puts("Successfully read #{songs.length / 2} songs.")
